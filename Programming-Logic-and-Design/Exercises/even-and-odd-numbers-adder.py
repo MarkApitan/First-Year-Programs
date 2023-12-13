1ae008d09bbb5ec6112e7c4ab308faf553d5625d
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2023-12-13 19:42:44 +0800

    fixed the output

--- Programming-Logic-and-Design/Exercises/even-and-odd-numbers-adder.py
+++ Programming-Logic-and-Design/Exercises/even-and-odd-numbers-adder.py
@@ -1,35 +1,46 @@
 
 #Assign number of attempt
 attempts = 4
+attempts_2 = 4
+even_sum=0
+odd_sum=0
+border = ("-------------------------------------------------------------------")
+
+#Welcome introduction of the program
+print (border)
+print("\tWelcome to EVEN and ODD numbers adder program")
+print (border)
+print ("This program calculates the sum of EVEN and ODD numbers you input")
+print (border)
 
 #This will loop 3 times if the inputted values is incorrect, if the 3 attempts ran out it will exit the program
 while attempts > 0:
     #To get the input from the user
-    number=input ("How Many Numbers: ")
-    print ("---------------------------------------")
+    number=input ("Enter how many numbers would you like to add: ")
+    print (border)
 
     #To check if the input is a number and has no decimal
     if number.isnumeric()==True and number.isdecimal()==True:
     
-        #To Check if the input number is not less than or equal to zero
-        if int(number) <=0:
+        #To Check if the input number is not less than or equal to one, I chose one because you cannot add just one number
+        if int(number) <=1:
             attempts = attempts-1
             #To display what is the error and how many attempts left
             if attempts > 1:
                 print ("Invalid Input! You cannot input numbers less than or equal to 0")
                 print ("You have " + str(attempts) + " attempts remaining")
-                print ("---------------------------------------")
+                print (border)
             elif attempts == 1:
                 print ("Invalid Input! You cannot input numbers less than or equal to 0")
                 print ("You have " + str(attempts) + " attempt remaining")
-                print ("---------------------------------------")
+                print (border)
             elif attempts == 0:
                 print ("You ran out of attempts")
                 exit()
-        #If the input is correct it will break the loop and display thank you message
+        #If the input is correct it will break the loop and display a message
         else:
-            print("Thank you for your cooperation")
-            print ("---------------------------------------")
+            print("Please enter the " + str(number) + " numbers")
+            print (border)
             break
     #If the input is not a number or has a decimal
     else:
@@ -38,18 +49,15 @@ while attempts > 0:
         if attempts > 1:
             print ("Invalid Input! Please Input a Number")
             print ("You have " + str(attempts) + " attempts remaining")
-            print ("---------------------------------------")
+            print (border)
         elif attempts == 1:
             print ("Invalid Input! Please Input a Number")
             print ("You have " + str(attempts) + " attempt remaining")
-            print ("---------------------------------------")
+            print (border)
         elif attempts == 0:
             print ("You ran out of attempts")
             exit()
 
-attempts_2 = 4
-even_sum=0
-odd_sum=0
 for x in range(int(number)):
     while attempts_2>0:
         num = input("Enter a Number: ")
@@ -63,16 +71,19 @@ for x in range(int(number)):
         else:
             attempts_2=attempts_2-1
             if attempts_2 > 1:
+                print (border)
                 print ("Invalid Input! Please Input a Number")
                 print ("You have " + str(attempts_2) + " attempts remaining")
-                print ("---------------------------------------")
+                print (border)
             elif attempts_2 == 1:
+                print (border)
                 print ("Invalid Input! Please Input a Number")
                 print ("You have " + str(attempts_2) + " attempt remaining")
-                print ("---------------------------------------")
+                print (border)
             elif attempts_2 == 0:
+                print (border)
                 print ("You ran out of attempts")
                 exit()
-    
+print (border)
 print ("The total sum of even numbers is " + str(even_sum))
 print ("The total sum of all odd numbers are " + str(odd_sum))
