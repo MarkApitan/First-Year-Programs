49d0bee09052c0f50f7cbee2eab82f128118e224
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2023-12-18 23:37:48 +0800

    changed the loop

--- Programming-Logic-and-Design/Exercises/even-and-odd-numbers-adder.py
+++ Programming-Logic-and-Design/Exercises/even-and-odd-numbers-adder.py
@@ -26,10 +26,9 @@ while True:
     #To assign values
     even_sum =0
     odd_sum =0
-    attempts = 4
     attempts_2 = 4
     #This will loop 4 times if the inputted values is incorrect, if the 4 attempts ran out it will exit the program
-    while attempts > 0:
+    while True:
         #To get the input from the user
         print (border)
         how_many_number = input("Enter how many numbers would you like to add: ")
@@ -40,16 +39,15 @@ while True:
             
             #To Check if the input number is not less than or equal to one, I chose one because you cannot add just one number
             if int(how_many_number) <=1:
-                attempts = attempts-1
                 #To display what is the error and how many attempts left
-                if attempts > 1:
-                    print ("\033[0;31mInvalid Input!\033[0m You cannot input numbers less than or equal to 1")
-                    print ("You have " + str(attempts) + " attempts remaining")
-                elif attempts == 1:
-                    print ("\033[0;31mInvalid Input!\033[0m You cannot input numbers less than or equal to 1")
-                    print ("You have " + str(attempts) + " attempt remaining")
-                elif attempts == 0:
-                    print ("\033[0;31mYou ran out of attempts\033[0m")
+                print ("\033[0;31mInvalid Input!\033[0m You cannot input numbers less than or equal to 1")
+                input_again = input ("Would you like to re-enter? (yes/no):")
+                if input_again.lower()=="yes":
+                    print (border)
+                else:
+                    print (border)
+                    print ("Thank you for using the Program")
+                    print (border)
                     exit()
             #If the input is correct it will break the loop and display a message
             else:
@@ -58,51 +56,118 @@ while True:
                 break
         #If the input is not a number or has a decimal
         else:
-            attempts=attempts-1
-            #To display what is the error and how many attempts left
-            if attempts > 1:
-                print ("\033[0;31mInvalid Input!\033[0m Please Input a Number")
-                print ("You have " + str(attempts) + " attempts remaining")
-            elif attempts == 1:
-                print ("\033[0;31mInvalid Input!\033[0m Please Input a Number")
-                print ("You have " + str(attempts) + " attempt remaining")
-            elif attempts == 0:
-                print ("\033[0;31mYou ran out of attempts\033[0m")
-                exit()
-                
-    #To loop n times the user inputted
-    for x in range(int(how_many_number)):
-        #This will loop 4 times if the inputted values are invalid, if the 4 attempts ran out it will exit the program
-        while attempts_2>0:
-            #to get the inputs
-            num_to_add = input("Enter a Number: ")
-            #to check if the inputs are valid
-            if is_int(num_to_add)==True:
-                #to check if the input are odd or even
-                if int(num_to_add)%2==0:
-                    #To get the sum of all even numbers
-                    even_sum=(int(even_sum)+int(num_to_add))
-                    break
-                else:
-                    #To get the sum of all odd numbers
-                    odd_sum=(int(odd_sum)+ int(num_to_add))
-                    break
+            print ("\033[0;31mInvalid Input!\033[0m Please enter a number")
+            input_again = input ("Would you like to re-enter? (yes/no):")
+
+            if input_again.lower()=="yes":
+                print (border)
             else:
-                attempts_2=attempts_2-1
-                if attempts_2 > 1:
+                print (border)
+                print ("Thank you for using the Program")
+                print (border)
+                exit()
+    for x in range (int(how_many_number)):
+        while True:            
+            nth_term = int(x)+1
+            if nth_term == 1:
+                num_to_add = input("Enter 1st number: ")
+
+                if is_int(num_to_add)==True:
+                    if int(num_to_add)%2==0:
+                        #To get the sum of all even numbers
+                        even_sum=(int(even_sum)+int(num_to_add))
+                        break
+                    else:
+                        #To get the sum of all odd numbers
+                        odd_sum=(int(odd_sum)+ int(num_to_add))
+                        break
+                else:
                     print (border)
-                    print ("\033[0;31mInvalid Input!\033[0m Please Input a Number")
-                    print ("You have " + str(attempts_2) + " attempts remaining")
+                    print ("\033[0;31mInvalid Input!\033[0m Please enter a number")
+                    input_again = input ("Would you like to re-enter? (yes/no):")
+
+                    if input_again.lower()=="yes":
+                        print (border)
+                    else:
+                        print (border)
+                        print ("Thank you for using the Program")
+                        print (border)
+                        exit()
+            if nth_term == 2:
+                num_to_add = input("Enter 2nd number: ")
+                
+                if is_int(num_to_add)==True:
+                    if int(num_to_add)%2==0:
+                        #To get the sum of all even numbers
+                        even_sum=(int(even_sum)+int(num_to_add))
+                        break
+                    else:
+                        #To get the sum of all odd numbers
+                        odd_sum=(int(odd_sum)+ int(num_to_add))
+                        break
+                else:
+                    print(border)
+                    print ("\033[0;31mInvalid Input!\033[0m Please enter a number")
+                    input_again = input ("Would you like to re-enter? (yes/no):")
+
+                    if input_again.lower()=="yes":
+                        print (border)
+                    else:
+                        print (border)
+                        print ("Thank you for using the Program")
+                        print (border)
+                        exit()
+
+            if nth_term == 3:
+                num_to_add = input("Enter 3rd number: ")
+
+                if is_int(num_to_add)==True:
+                    if int(num_to_add)%2==0:
+                        #To get the sum of all even numbers
+                        even_sum=(int(even_sum)+int(num_to_add))
+                        break
+                    else:
+                        #To get the sum of all odd numbers
+                        odd_sum=(int(odd_sum)+ int(num_to_add))
+                        break
+                else:
                     print (border)
-                elif attempts_2 == 1:
+                    print ("\033[0;31mInvalid Input!\033[0m Please enter a number")
+                    input_again = input ("Would you like to re-enter? (yes/no):")
+
+                    if input_again.lower()=="yes":
+                        print (border)
+                    else:
+                        print (border)
+                        print ("Thank you for using the Program")
+                        print (border)
+                        exit()
+            if nth_term>3:
+                num_to_add = input("Enter "+ str(nth_term) + "th number: ")
+
+                if is_int(num_to_add)==True:
+                    if int(num_to_add)%2==0:
+                        #To get the sum of all even numbers
+                        even_sum=(int(even_sum)+int(num_to_add))
+                        break
+                    else:
+                        #To get the sum of all odd numbers
+                        odd_sum=(int(odd_sum)+ int(num_to_add))
+                        break
+                else:
                     print (border)
-                    print ("\033[0;31mInvalid Input!\033[0m Please Input a Number")
-                    print ("You have " + str(attempts_2) + " attempt remaining")
-                    print (border)
-                elif attempts_2 == 0:
-                    print (border)
-                    print ("\033[0;31mYou ran out of attempts\033[0m")
-                    exit()
+                    print ("\033[0;31mInvalid Input!\033[0m Please enter a number")
+                    input_again = input ("Would you like to re-enter? (yes/no):")
+
+                    if input_again.lower()=="yes":
+                        print (border)
+                    else:
+                        print (border)
+                        print ("Thank you for using the Program")
+                        print (border)
+                        exit()
+                
+            
     #To display the sum
     print (border)
     print ("The total sum of even numbers is " + str(even_sum))
