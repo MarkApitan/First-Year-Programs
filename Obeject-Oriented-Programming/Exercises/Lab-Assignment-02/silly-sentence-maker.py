d50ad1e6a963abd88c1f373a6188c41baa6c3c7b
Mark Justine L. Apitan <markjustinelapitan@iskolarngbayan.pup.edu.ph>
2024-03-16 20:50:48 +0800

    Finished Lab Assignment no.2

---
+++ Obeject-Oriented-Programming/Exercises/Lab-Assignment-02/silly-sentence-maker.py
@@ -0,0 +1,22 @@
+#Lab Exercise No 2: Problem No. 4
+#Programmed by: Mark Justine L. Apitan
+#Course, Year and Section: BSCpE 1-3
+#Instructor: Engr. Julius S. Cansino
+#Date Submitted:March 16, 2024
+
+#Introduction 
+border = ("-----------------------------------------------------------------------------------------------------------------------------------")
+print("\nProgram Title: Lab Exercise No 2 | Problem No. 4 \nProgrammed by: Mark Justine L. Apitan")
+print(border)
+
+#getting the inputs
+name = input("Enter a name: ")
+adjective = input("Enter an adjective: ")
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adverb = input("Enter an adverb: ")
+
+#making the sentence
+silly_sentence = ("\nIn a land far, far away, " + name + " " + adverb + " "  +verb + " with a " + adjective + " " +  noun + "!")
+print(silly_sentence)
+print(border)
